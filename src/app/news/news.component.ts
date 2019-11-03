db0aa52820cec635c3df495efaca63be39ddc686
srinivashyd77 <srinivashyd77@gmail.com>
2019-11-03 23:25:16 +0000

    initial commit

---
+++ src/app/news/news.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.scss']
+})
+export class NewsComponent implements OnInit {
+  articles;
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getNews().subscribe((res)=>{
+      console.log(res);
+      this.articles = res;
+    });
+  }
+
+}
